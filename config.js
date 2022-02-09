658b06f781fe6622f8cd5f9d109ba2a5d7002b53
Scribeguru <pocomitch@gmail.com>
2022-02-09 15:47:36 -0800

    temporarily removed config from gitignore

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'secretKey': '47f3afc0d77922059e96817e4c668d0107b749cea682b2297ec99b13e3fcc4d584ad23ed8bf16f922e9e0ad63ec97bb5',
+  'mongoUrl': 'mongodb+srv://mitchpoco:OPHANIMIC__lamborghini66@cluster0.fnzg1.mongodb.net/rockstar-elite?retryWrites=true&w=majority'
+};
+
